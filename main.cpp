7cd48bd987b246743896c4c3131abb67b6cdb407
Alex Prasov <freezay00828002@gmail.com>
2025-03-05 12:55:46 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,152 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <vector>
+#include <iostream>
+
+
+using namespace sf;
+using namespace std;
+
+
+const int SIZE = 4; // Розмір поля 4x4
+const int TILE_SIZE = 100; // Розмір кожної плитки
+const int WINDOW_SIZE = TILE_SIZE * SIZE; // Розмір вікна
+
+
+class Fifteens {
+
+private:
+    vector<int>tiles;
+
+public:
+    int size = 4; // By default 4 x 4
+    
+
+    // Constructor
+    Fifteens() {
+        reset();
+    }
+
+    void reset()
+    {
+        tiles.clear();
+        for (size_t i = 0; i < size * size - 1; ++i)
+        {
+            tiles.push_back(i+1);
+        }
+        tiles.push_back(0);
+    }
+
+    void showTilesOnScreen(RenderWindow& window)
+    {
+        for (size_t i = 0; i < size * size; ++i)
+        {
+            int X_pos = (i % size) * TILE_SIZE;
+            int Y_pos = (i / size) * TILE_SIZE;
+
+            // Створення об'єкту "плитка"
+            RectangleShape tile(Vector2f(TILE_SIZE, TILE_SIZE));
+
+            // Встановлюємо плитку на відповідне місце
+            tile.setPosition(X_pos, Y_pos);
+
+            // Задаємо параметри для кожної плитки
+            tile.setOutlineColor(Color::Black);
+            tile.setOutlineThickness(3);
+
+
+            if (tiles[i] != 0) {
+
+                tile.setFillColor(Color::Cyan);
+
+                // Створюємо шрифт "Arial"
+                Font font;
+                if (!font.loadFromFile("Fonts/Arial.ttf")) {
+                    cerr << "Error loading font!" << endl;
+                }
+
+                // Створюємо шрифт текст із номером плитки
+                Text text(to_string(tiles[i]), font, 30);
+
+                // Встановлюємо колір 
+                text.setFillColor(sf::Color::Black);
+
+                // Встановлюємо текст по середины плитки із урахуванням товщини бордерів
+                text.setPosition(X_pos + TILE_SIZE / 2 - text.getLocalBounds().width / 2,
+                    Y_pos + TILE_SIZE / 2 - text.getLocalBounds().height / 2);
+
+
+
+                // відображаємо плитку і текст
+                window.draw(tile);
+                window.draw(text);
+            }
+
+            else {
+
+                tile.setFillColor(Color::White);
+                tile.setOutlineColor(Color::Black);
+                tile.setOutlineThickness(3);
+
+                window.draw(tile);
+            }
+        }
+           
+    }
+
+};
+
+
+
+int main() {
+
+    // Створюємо вікно розміру 800x600
+    RenderWindow window(VideoMode(WINDOW_SIZE, WINDOW_SIZE), "SFML Window Fifteen Game");
+    
+    
+
+    // Sprite creation
+
+    /*Texture texture;
+    if (!texture.loadFromFile("mySprite.jpg")) return EXIT_FAILURE;
+    
+    Sprite sprite(texture);*/
+
+
+    // Text
+
+   /* Font font;
+    if (!font.loadFromFile("arial.ttf"))
+        return EXIT_FAILURE;
+    Text text("Hello SFML", font, 50);*/
+   
+
+
+    // Об'єкт для обробки подій
+    Event event;
+
+    Fifteens myFifteens;
+
+    // Основний цикл
+    while (window.isOpen()) {
+        // Обробка подій
+        // window.pollEvent(event) — функція, яка витягує події з черги подій, що виникли у вікні. 
+        // Вона повертає true, якщо є нові події.
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close(); // Закриваємо вікно при натисканні "Закрити"
+            }
+        }
+
+        // Очищаємо вікно чорним кольором
+        window.clear(Color::Black);
+
+        // Малюємо коло на екрані
+        myFifteens.showTilesOnScreen(window);
+
+        // Оновлюємо вікно
+        window.display();
+    }
+
+    return 0;
+}
